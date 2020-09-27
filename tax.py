135f8d3f1a4a69fa7bdb41dcdf70ca668de90699
Matt Carey <matt@authorityspoke.com>
2020-09-27 03:17:14 -0500

    calculate_education_tax_credit is separate function

--- tax.py
+++ tax.py
@@ -1,11 +1,17 @@
+def calculate_education_tax_credit(salary: int) -> int:
+    """Calculate education tax credit per child."""
+    if salary < 20000:
+        credit_per_child = 800
+    elif 20000 <= salary < 22000:
+        credit_per_child = 800 - salary % 20000 * 0.2
+    else:
+        credit_per_child = 400
+    return int(credit_per_child)
+
+
 def calculate_tax(salary: int, num_children: int) -> int:
     """Calculate income tax, so simply you could file it on a postcard."""
-    if salary < 21000:
-        scholarship_per_child = 800
-    elif 21000 <= salary < 22000:
-        scholarship_per_child = 800 - salary % 21000 * 0.2
-    else:
-        scholarship_per_child = 600
+    credit_per_child = calculate_education_tax_credit(salary)
 
-    total_tax = salary * 0.5 - num_children * scholarship_per_child
+    total_tax = salary * 0.5 - num_children * credit_per_child
     return int(total_tax)
--- test_hypo.py
+++ test_hypo.py
@@ -1,13 +1,14 @@
-from hypothesis import given, strategies
+from hypothesis import given, strategies, settings
 
 from tax import calculate_tax
 
 
 @given(
-    salary=strategies.integers(),
+    salary=strategies.integers(min_value=0, max_value=200000),
     salary_raise=strategies.integers(min_value=1),
-    num_children=strategies.integers(),
+    num_children=strategies.integers(min_value=0, max_value=10),
 )
+@settings(max_examples=10000)
 def test_raise_in_salary_never_causes_a_loss(salary, salary_raise, num_children):
     """
     Test that a raise in salary never triggers a tax increase greater than the raise.
@@ -20,4 +21,5 @@ def test_raise_in_salary_never_causes_a_loss(salary, salary_raise, num_children)
         salary=salary + salary_raise, num_children=num_children
     )
     increased_tax = income_tax_after_raise - income_tax_without_raise
+
     assert increased_tax <= salary_raise
